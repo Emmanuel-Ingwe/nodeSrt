32aeb1a838d4af0475bbd917cd474a8a2efa41ff
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2022-08-06 01:24:08 +0100

    node/console

---
+++ index.js
@@ -0,0 +1,5 @@
+// console.log('Hello From Node.js....');``
+
+const person = require('./person');
+
+console.log('person.name');
---
+++ person.js
@@ -0,0 +1,6 @@
+const person = {
+    name: 'john Doe',
+    age: '65'
+};
+
+module.exports = person;
