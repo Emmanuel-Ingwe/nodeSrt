29ecb740d4747623e6de2dcd210115eb5ae86835
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2022-08-07 20:55:33 +0100

    fs_demo

--- reference/path.demo.js
+++ reference/path.demo.js
@@ -2,3 +2,13 @@ const path = require('path');
 
 // BASE FILE
 console.log(path.basename(__filename));
+
+// Directory name
+console.log(path.dirname(__filename));
+
+console.log(path.extname(__filename));
+
+console.log(path.parse(__filename));
+
+// CONCATENATE PATHS
+console.log(path.join(__dirname, 'test', 'hello.html'));
