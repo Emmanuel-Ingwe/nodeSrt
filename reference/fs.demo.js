16e321233e23e988a15e468c1f032c7150f88619
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2022-08-11 23:19:56 +0100

    adding

--- reference/fs.demo.js
+++ reference/fs.demo.js
@@ -1,8 +1,14 @@
-const fs = require('fs');
-const path = require('path');
+// const fs = require('fs');
+// const path = require('path');
 
-// CREATE FOLD.
-fs.mkdir(path.join(__dirname, '/test'), {}, (err) => {
-    if (err) throw err;
-    console.log('Folder created...');
-});
+// // CREATE FOLD.
+// fs.mkdir(path.join(__dirname, '/test'), {}, (err) => {
+//     if (err) throw err;
+//     console.log('Folder created...');
+// });
+
+function add(val1, val2) {
+    return val1 + val2;
+}
+
+console.log(add(50, 66));
